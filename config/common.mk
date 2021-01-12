f48d07dd87907ae40973c322ce8ae4a8d8160cfc
Immanuel Raj <immanuelr44@gmail.com>
2021-01-12 18:22:24 +0530

    Disable blurs by default
    Since We focus on performance we dont need this by default if they need they can enable in the device settings or if they need it out of the box developers can add a PRODUCT_PRODUCT_PROPERTIES and set this persist.sys.sf.disable_blurs=0 via makefiles in device tree

--- config/common.mk
+++ config/common.mk
@@ -201,5 +201,11 @@ ifeq ($(WITH_GAPPS), true)
 endif
 endif
 
+# Disable blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Include Vendor Parts
 -include vendor/LegionParts/parts.mk
