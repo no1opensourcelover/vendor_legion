54992fd650df525aa293bba6ec0b8cf60a1c37c7
Immanuel Raj <immanuelr44@gmail.com>
2021-02-17 17:06:27 +0530

    Vendor: Add new scripts to make changelog
    Used to make changelog of the whole rom
    
    This collects all the recent commits and makes it as a txt file containing all changes
    
    Originally by jhenrique for pe adapted to freakyOS by bunny adapted to legionos by Immanuel

--- tools/changelog.sh
+++ tools/changelog.sh
@@ -4,32 +4,29 @@ export Changelog=$PWD/Changelog.txt
 
 if [ -f $Changelog ];
 then
-    rm -f $Changelog
+	rm -f $Changelog
 fi
+
+export formatter_script=$PWD/changelog_repo_formatter.sh
 
 touch $Changelog
 
+echo "Generating changelog..."
+
 for i in $(seq 14);
 do
-export After_Date=`date --date="$i days ago" +%F`
+export After_Date=`date --date="$i days ago" +%Y/%m/%d`
 k=$(expr $i - 1)
-export Until_Date=`date --date="$k days ago" +%F`
-echo "====================" >> $Changelog;
-echo "     $Until_Date    " >> $Changelog;
-echo "====================" >> $Changelog;
-# Cycle through every repo to find commits between 2 dates
-while read path;
-        do
-    CLOG=$(git --git-dir ./${path}/.git log --oneline --after=$After_Date --until=$Until_Date);
-    if [ -n "$CLOG" ]; then
-       echo project $path >> $Changelog
-       git --git-dir ./${path}/.git log --oneline --after=$After_Date --until=$Until_Date >> $Changelog
-    fi
-done < ./.repo/project.list;
-echo "" >> $Changelog;
+	export Until_Date=`date --date="$k days ago" +%Y/%m/%d`
+
+	# Line with after --- until was too long for a small ListView
+	echo '=======================' >> $Changelog;
+	echo  "     "$Until_Date       >> $Changelog;
+	echo '=======================' >> $Changelog;
+	echo >> $Changelog;
+
+	# Cycle through every repo to find commits between 2 dates
+	repo forall -c '. $formatter_script' >> $Changelog
+	echo >> $Changelog;
 done
-
-sed -i 's/project/ */g' $Changelog
-sed -i 's/[/]$//' $Changelog
-
-cp $Changelog $OUT/
+echo "Chnagelog generated succesfully..Yay!!!"
