8aca7799ac874af69722887c6f7fc5553c7563a1
Immanuel Raj <immanuelr44@gmail.com>
2021-03-01 18:37:07 +0530

    version.mk: Move to 3.7

--- config/version.mk
+++ config/version.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 ANDROID_VERSION := 11.0
-LEGIONVERSION := v3.6
+LEGIONVERSION := v3.7
 
 LEGION_BUILD_TYPE ?= UNOFFICIAL
 LEGION_BUILD_DATE := $(shell date +%Y%m%d-%H%M)
