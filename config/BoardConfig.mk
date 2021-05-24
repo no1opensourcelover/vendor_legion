dbe694a3ea2e35d175ec455502c25b9d47cf61c6
Immanuel Raj <immanuelr44@gmail.com>
2021-05-25 04:11:22 +0530

    Remove some derped declaration
    This already exists in boardconfiglegion.mk

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,14 +1,6 @@
 # Safetynet
 TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
 
-include vendor/aosp/config/BoardConfigKernel.mk
-
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/aosp/config/BoardConfigQcom.mk
-endif
-
-include vendor/aosp/config/BoardConfigSoong.mk
-
 # Namespace for fwk-detect
 TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
 PRODUCT_SOONG_NAMESPACES += \
