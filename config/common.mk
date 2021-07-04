maldini03/vendor_banana

vendor: Include makefile with art definitions
Change-Id: Ied15be62e315db3061dfb216ea6c7f0fdb43aa22
Signed-off-by: mydongistiny <jaysonedson@gmail.com>

config/common.mk
@@ -3,6 +3,9 @@ BANANA_BUILD := true
 
 BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210605.005/7349499:user/release-keys
 
+# Inherit art options
+include vendor/banana/config/art.mk
+
 ifeq ($(PRODUCT_GMS_CLIENTID_BASE),)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.com.google.clientidbase=android-google

no1opensourcelover/vendor_legion

Signed-off-by: Joey Huab <joey@evolution-x.org>

config/common.mk
@@ -249,3 +249,6 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Include Vendor Parts
 -include vendor/LegionParts/parts.mk
+
+# Inherit art options
+include vendor/legion/config/art.mk