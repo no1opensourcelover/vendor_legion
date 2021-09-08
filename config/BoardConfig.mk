9c3d4a126c0a3f6910e4843eec0452291f58177c
IMMANUEL44 <immanuelr44@gmail.com>
2021-09-08 23:24:27 +0530

    Vendor: Switch to pixel 5a september fp

--- config/BoardConfig.mk
+++ config/BoardConfig.mk
@@ -1,5 +1,5 @@
 # Safetynet
-TARGET_FORCE_BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+TARGET_FORCE_BUILD_FINGERPRINT := google/barbet/barbet:11/RD2A.210905.002/7513089:user/release-keys
 
 # Namespace for fwk-detect
 TARGET_FWK_DETECT_PATH ?= hardware/qcom-caf/common
